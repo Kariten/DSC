c23d9e25f46a20e3416b8c5255ce074b3fc1dcce
mx <1979646055@qq.com>
2021-05-09 17:19:36 +0800

    add requirements.txt

---
+++ util/imageCode.py
@@ -0,0 +1,59 @@
+import random
+import string
+from PIL import Image, ImageDraw, ImageFont
+from flask import make_response, session
+from io import BytesIO
+
+
+def geneText():
+    """生成4位验证码"""
+    varstr = ''.join(random.sample(string.ascii_letters + string.digits, 4))  # ascii_letters是生成所有字母 digits是生成所有数字0-9
+    return varstr
+
+
+def rndColor():
+    """随机颜色"""
+    return random.randint(32, 127), random.randint(32, 127), random.randint(32, 127)
+
+
+def drawLines(draw, num, width, height):
+    """划线"""
+    for num in range(num):
+        x1 = random.randint(0, width / 2)
+        y1 = random.randint(0, height / 2)
+        x2 = random.randint(0, width)
+        y2 = random.randint(height / 2, height)
+        draw.line(((x1, y1), (x2, y2)), fill='black', width=1)
+
+
+def getVerifyCode():
+    """生成验证码图形"""
+    code = geneText()
+    # 图片大小为偶数
+    width, height = 120, 40
+    # 新图片对象
+    im = Image.new('RGB', (width, height), 'white')
+    # 字体
+    font = ImageFont.truetype('app/static/arial.ttf', 35)
+    # draw对象
+    draw = ImageDraw.Draw(im)
+    # 绘制字符串
+    for item in range(4):
+        draw.text((5 + random.randint(-3, 3) + 23 * item, 5 + random.randint(-3, 3)),
+                  text=code[item], fill=rndColor(), font=font)
+    drawLines(draw, 5, width, height)
+    return im, code
+
+
+def getImgCode():
+    image, code = getVerifyCode()
+    # 图片以二进制形式写入
+    buf = BytesIO()
+    image.save(buf, 'jpeg')
+    buf_str = buf.getvalue()
+    # 把buf_str作为response返回前端，并设置首部字段
+    response = make_response(buf_str)
+    response.headers['Content-Type'] = 'image/gif'
+    # 将验证码字符串储存在session中
+    session['imageCode'] = code
+    return response
